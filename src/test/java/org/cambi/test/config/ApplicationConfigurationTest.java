892534a6bc99617bb1990f72396b3696bb899952
lucacambi@rentalcars.com <lucacambi@rentalcars.com>
2020-04-27 23:44:33 +0200

    adds exception handling

--- src/test/java/org/cambi/test/config/ApplicationConfigurationTest.java
+++ src/test/java/org/cambi/test/config/ApplicationConfigurationTest.java
@@ -31,61 +31,64 @@ import static org.mockito.Mockito.when;
  */
 @Configuration
 @Profile("test")
-public class ApplicationConfigurationTest
-{
-	@Mock
-	private TwitterAuthenticator authenticator;
+public class ApplicationConfigurationTest {
+    @Mock
+    private TwitterAuthenticator authenticator;
 
-	@Autowired
-	private ObjectMapper objectMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-	@Bean
-	public TwitterAuthenticator getTwitterAuthenticator() throws TwitterAuthenticationException, IOException {
-		MockitoAnnotations.initMocks(this);
+    @Bean
+    public TwitterAuthenticator getTwitterAuthenticator() throws TwitterAuthenticationException, IOException {
+        MockitoAnnotations.initMocks(this);
 
-		/**
-		 * We read tweets from a file to create a fake request
-		 *
-		 * Mocking google Transport
-		 */
-		HttpTransport transport = new MockHttpTransport() {
-			@Override
-			public LowLevelHttpRequest buildRequest(String method, String url) throws IOException {
-				return new MockLowLevelHttpRequest() {
-					@Override
-					public LowLevelHttpResponse execute() throws IOException {
+        /**
+         * We read tweets from a file to create a fake request
+         *
+         * Mocking google Transport
+         */
+        HttpTransport transport = new MockHttpTransport() {
+            @Override
+            public LowLevelHttpRequest buildRequest(String method, String url) throws IOException {
+                return new MockLowLevelHttpRequest() {
+                    @Override
+                    public LowLevelHttpResponse execute() throws IOException {
 
-						InputStream is = new FileInputStream("src/test/resources/tweets.json");
-						BufferedReader buf = new BufferedReader(new InputStreamReader(is));
+                        try {
+                            InputStream is = new FileInputStream("src/test/resources/tweets.json");
+                            BufferedReader buf = new BufferedReader(new InputStreamReader(is));
 
-						String line = buf.readLine();
-						StringBuilder sb = new StringBuilder();
+                            String line = buf.readLine();
+                            StringBuilder sb = new StringBuilder();
 
-						while (line != null) {
-							sb.append(line).append("\n");
-							line = buf.readLine();
-						}
-						buf.close();
+                            while (line != null) {
+                                sb.append(line).append("\n");
+                                line = buf.readLine();
+                            }
+                            buf.close();
 
-						StatusRun status = objectMapper.readValue(sb.toString(), StatusRun.class);
-						sb = new StringBuilder();
+                            StatusRun status = objectMapper.readValue(sb.toString(), StatusRun.class);
+                            sb = new StringBuilder();
 
-						for (TweetRun tweet : status.getTweets()) {
-							sb.append(objectMapper.writeValueAsString(tweet)).append("\n");
-						}
+                            for (TweetRun tweet : status.getTweets()) {
+                                sb.append(objectMapper.writeValueAsString(tweet)).append("\n");
+                            }
 
-						MockLowLevelHttpResponse response = new MockLowLevelHttpResponse();
-						response.setStatusCode(200);
-						response.setContentType(Json.MEDIA_TYPE);
-						response.setContent(sb.toString());
-						return response;
-					}
-				};
-			}
-		};
+                            MockLowLevelHttpResponse response = new MockLowLevelHttpResponse();
+                            response.setStatusCode(200);
+                            response.setContentType(Json.MEDIA_TYPE);
+                            response.setContent(sb.toString());
+                            return response;
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                };
+            }
+        };
 
-		when(authenticator.getAuthorizedHttpRequestFactory()).thenReturn(transport.createRequestFactory());
-		return authenticator;
-	}
+        when(authenticator.getAuthorizedHttpRequestFactory()).thenReturn(transport.createRequestFactory());
+        return authenticator;
+    }
 
 }
