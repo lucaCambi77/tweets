415c4201da00d25a04db9691d9a360a1200d93c0
lucacambi@rentalcars.com <lucacambi@rentalcars.com>
2020-04-27 16:40:30 +0200

    adds required input

--- src/main/java/org/cambi/utils/Utils.java
+++ src/main/java/org/cambi/utils/Utils.java
@@ -3,6 +3,7 @@
  */
 package org.cambi.utils;
 
+import org.cambi.model.Run;
 import org.cambi.model.TweetRun;
 
 import java.util.*;
@@ -14,32 +15,35 @@ import java.util.stream.Collectors;
  */
 public class Utils {
 
-	/**
-	 * @param tweets
-	 * @return
-	 */
-	public static LinkedHashMap<Object, List<TweetRun>> sortTweets(Set<TweetRun> tweets) {
+    /**
+     * @param tweets
+     * @return
+     */
+    public static LinkedHashMap<Object, List<TweetRun>> sortTweets(Set<TweetRun> tweets) {
 
-		LinkedHashMap<Object, List<TweetRun>> tweetByUser = tweets.stream().sorted(new Comparator<TweetRun>() {
-			@Override
-			public int compare(final TweetRun lhs, TweetRun rhs) {
-				long dateleft = lhs.getUserTweet() == null ? 0 : lhs.getUserTweet().getCreationDate().getTime();
-				long dateRight = rhs.getUserTweet() == null ? 0 : rhs.getUserTweet().getCreationDate().getTime();
+        Objects.requireNonNull(tweets);
 
-				return Long.signum(dateleft - dateRight);
-			}
-		}).collect(Collectors.groupingBy(
-				p -> Optional.ofNullable(p.getUserTweet() == null ? null : p.getUserTweet().getId().getUserId()),
-				LinkedHashMap::new, Collectors.toList()));
+        LinkedHashMap<Object, List<TweetRun>> tweetByUser = tweets.stream().sorted(new Comparator<TweetRun>() {
+            @Override
+            public int compare(final TweetRun lhs, TweetRun rhs) {
+                long dateleft = lhs.getUserTweet() == null ? 0 : lhs.getUserTweet().getCreationDate().getTime();
+                long dateRight = rhs.getUserTweet() == null ? 0 : rhs.getUserTweet().getCreationDate().getTime();
 
-		for (List<TweetRun> userTweetList : tweetByUser.values()) {
-			userTweetList.sort(new Comparator<TweetRun>() {
-				@Override
-				public int compare(final TweetRun lhs, TweetRun rhs) {
-					return Long.signum(lhs.getCreationDate().getTime() - rhs.getCreationDate().getTime());
-				}
-			});
-		}
-		return tweetByUser;
-	}
+                return Long.signum(dateleft - dateRight);
+            }
+        }).collect(Collectors.groupingBy(
+                p -> Optional.ofNullable(p.getUserTweet() == null ? null : p.getUserTweet().getId().getUserId()),
+                LinkedHashMap::new, Collectors.toList()));
+
+        for (List<TweetRun> userTweetList : tweetByUser.values()) {
+            userTweetList.sort(new Comparator<TweetRun>() {
+                @Override
+                public int compare(final TweetRun lhs, TweetRun rhs) {
+                    return Long.signum(lhs.getCreationDate().getTime() - rhs.getCreationDate().getTime());
+                }
+            });
+        }
+        return tweetByUser;
+    }
+
 }
